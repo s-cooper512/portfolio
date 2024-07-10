8963ad2c9255017e16de7aec04ba6a0b80ec52cb
Stephen Cooper <scooper@indeed.com>
2024-07-10 17:56:50 -0500

    Adjusted aside 2

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 function Aside() {
   return (
     <aside>
+      <div className='project-grid'>
       <div>
       <h3>Programming Experience</h3>
       <ul>
@@ -18,7 +19,8 @@ function Aside() {
       </ul>
       </div>
       <div>
-        <img src="GACert.png" alt="General Assembly Programming Certificate" />
+        <img src="GACert.png" alt="General Assembly Programming Certificate" height="300px" width="100%"/>
+        </div>
       </div>
     </aside>
   );
